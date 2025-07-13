40b1185dc64cdcc17da77dd0b2f0f3c4bfc5f10f
Jeff97ares <92552303+Jeff97ares@users.noreply.github.com>
2025-07-13 17:48:41 -0500

    Delete backend directory

--- backend/src/routes/rating.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createRating, getAllRatings } from '../controllers/rating.controller.js';
-
-const router = express.Router();
-router.post('/', createRating);
-router.get('/', getAllRatings);
-
-export default router;
